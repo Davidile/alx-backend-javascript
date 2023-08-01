63704338ec66dec9f4450bf940b13f540fa527b4
david <mugomadavid24@gmail.com>
2023-08-01 08:57:37 -0700

    manipulating data in js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(testSet, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
+  const res = [];
+  for (const element of testSet) {
+    if (typeof element === 'string' && element.startsWith(startString)) {
+      res.push(element.substring(startString.length));
+    }
+  }
+
+  return res.join('-');
+}
